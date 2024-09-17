7df0c13790c362daf2fb613cd2d4db4396d2b24a
CollinWilkerson <wilkerc2@my.erau.edu>
2024-09-16 19:31:13 -0700

    pg33

---
+++ BattleRoyaleDev/Assets/Scripts/Menu.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Menu : MonoBehaviourPunCallbacks, ILobbyCallbacks
+{
+    //UI Containers
+    [Header("Screens")]
+    public GameObject mainScreen;
+    public GameObject createRoomScreen;
+    public GameObject lobbyScreen;
+    public GameObject lobbyBrowserScreen;
+
+    [Header("Main Screen")]
+    public Button createRoomButton;
+    public Button findRoomButton;
+
+    //lobby text to change
+    [Header("Lobby")]
+    public TextMeshProUGUI playerListText;
+    public TextMeshProUGUI roomInfoText;
+    public Button startGameButton;
+
+    //extesible lists to hold a changing number of room buttons
+    [Header("Lobby Browser")]
+    public RectTransform roomListContainer;
+    public GameObject roomButtonPrefab;
+
+    private List<GameObject> roomButtons = new List<GameObject>();
+    private List<RoomInfo> roomList = new List<RoomInfo>();
+
+    private void Start()
+    {
+        //disable menus that aren't start
+        mainScreen.SetActive(true);
+        createRoomScreen.SetActive(false);
+        lobbyScreen.SetActive(false);
+        lobbyBrowserScreen.SetActive(false);
+
+        //disable buttons until connected
+        createRoomButton.interactable = false;
+        findRoomButton.interactable = false;
+
+        //make sure the cursor is free as it is locked during gameplay
+        Cursor.lockState = CursorLockMode.None;
+
+        //if in room from previous game
+        if (PhotonNetwork.InRoom)
+        {
+            PhotonNetwork.CurrentRoom.IsVisible = true;
+            PhotonNetwork.CurrentRoom.IsOpen = true;
+        }
+    }
+
+    /// <summary>
+    /// disables all screens then renables passed screen
+    /// </summary>
+    /// <param name="screen"></param>
+    private void SetScreen(GameObject screen)
+    {
+        
+        mainScreen.SetActive(false);
+        createRoomScreen.SetActive(false);
+        lobbyScreen.SetActive(false);
+        lobbyBrowserScreen.SetActive(false);
+
+        screen.SetActive(true);
+    }
+}
